9520c9e2ecc8b949c5acbe7e516cfbee243d02bc
Marta <martadipuccio@me.com>
2018-07-05 15:25:00 +0200

    contact js animation

--- index.js
+++ index.js
@@ -2,3 +2,107 @@ AOS.init({
     duration:2000
 });
 
+
+var Messenger = function(el){
+    'use strict';
+    var m = this;
+    
+    m.init = function(){
+      m.codeletters = "&#*+%?£@§$";
+      m.message = 0;
+      m.current_length = 0;
+      m.fadeBuffer = false;
+      m.messages = [
+        'Hi there! This is the contact section.',
+        'Here you can get in touch with me.',
+        'I will be happy to hear from you!',
+        'Cool.'
+      ];
+      
+      setTimeout(m.animateIn, 100);
+    };
+    
+    m.generateRandomString = function(length){
+      var random_text = '';
+      while(random_text.length < length){
+        random_text += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+      } 
+      
+      return random_text;
+    };
+    
+    m.animateIn = function(){
+      if(m.current_length < m.messages[m.message].length){
+        m.current_length = m.current_length + 2;
+        if(m.current_length > m.messages[m.message].length) {
+          m.current_length = m.messages[m.message].length;
+        }
+        
+        var message = m.generateRandomString(m.current_length);
+        $(el).html(message);
+        
+        setTimeout(m.animateIn, 20);
+      } else { 
+        setTimeout(m.animateFadeBuffer, 20);
+      }
+    };
+    
+    m.animateFadeBuffer = function(){
+      if(m.fadeBuffer === false){
+        m.fadeBuffer = [];
+        for(var i = 0; i < m.messages[m.message].length; i++){
+          m.fadeBuffer.push({c: (Math.floor(Math.random()*12))+1, l: m.messages[m.message].charAt(i)});
+        }
+      }
+      
+      var do_cycles = false;
+      var message = ''; 
+      
+      for(var i = 0; i < m.fadeBuffer.length; i++){
+        var fader = m.fadeBuffer[i];
+        if(fader.c > 0){
+          do_cycles = true;
+          fader.c--;
+          message += m.codeletters.charAt(Math.floor(Math.random()*m.codeletters.length));
+        } else {
+          message += fader.l;
+        }
+      }
+      
+      $(el).html(message);
+      
+      if(do_cycles === true){
+        setTimeout(m.animateFadeBuffer, 50);
+      } else {
+        setTimeout(m.cycleText, 2000);
+      }
+    };
+    
+    m.cycleText = function(){
+      m.message = m.message + 1;
+      if(m.message >= m.messages.length){
+        m.message = 0;
+      }
+      
+      m.current_length = 0;
+      m.fadeBuffer = false;
+      $(el).html('');
+      
+      setTimeout(m.animateIn, 200);
+    };
+    
+    m.init();
+  }
+  
+  console.clear();
+  var messenger = new Messenger($('#messenger'));
+
+
+/* Copyright (c) 2018 by Lauri (https://codepen.io/bionik/pen/dzBweB)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
